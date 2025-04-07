cd3f6d6072ad5f8bcd8798b57fca1348ab3ecd0a
sherryadan <adanmohammad80@gmail.com>
2025-04-07 19:52:28 +0500

    Sign Up Page Update

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -4,7 +4,7 @@ import "../globals.css";
 export default function Signup() {
   return (
     <main className="max-w-4xl mx-auto mt-4 px-4 sm:px-6 lg:px-8">
-            <Form />
+      <Form />
     </main>
   );
 }
