8eed0e6ba25902d655838c25c942501a0e5e41a4
sherryadan <adanmohammad80@gmail.com>
2025-04-09 02:24:39 +0500

    Middleware update

--- src/middleware.js
+++ src/middleware.js
@@ -1,27 +1,34 @@
 import { NextResponse } from "next/server";
 import { verifyToken } from "../lib/auth";
-
 export function middleware(req) {
+  const { pathname } = req.nextUrl;
+
+  if (
+    pathname.startsWith("/login") ||
+    pathname.startsWith("/_next") || 
+    pathname.startsWith("/static") || 
+    // pathname.startsWith("/") || 
+    pathname.startsWith("/api")
+  ) {
+    return NextResponse.next();
+  }
+
   const tokenCookie = req.cookies.get("authToken");
   const token = tokenCookie?.value;
-  // console.log("Token from cookie:", token);
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
   try {
-    verifyToken(token); 
-    return NextResponse.next();
+    verifyToken(token);
+    return NextResponse.redirect(new URL("/", req.url));
   } catch (error) {
-    return NextResponse.json(
-      { error: "Unauthorized: Invalid or expired token" },
-      { status: 401 }
-    );
+    return NextResponse.redirect(new URL("/login", req.url));
   }
 }
 
 export const config = {
-    runtime: 'nodejs',
+  runtime: "nodejs",
   matcher: ["/:path*"], 
 };
