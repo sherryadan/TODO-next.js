9ac1e574a792ff91d75a2e0d98116c8068b9cd04
sherryadan <adanmohammad80@gmail.com>
2025-04-09 21:20:06 +0500

    Middleware Update

--- src/middleware.js
+++ src/middleware.js
@@ -1,34 +1,35 @@
 import { NextResponse } from "next/server";
 import { verifyToken } from "../lib/auth";
 export function middleware(req) {
-  const { pathname } = req.nextUrl;
-
-  if (
-    pathname.startsWith("/login") ||
-    pathname.startsWith("/_next") || 
-    pathname.startsWith("/static") || 
-    // pathname.startsWith("/") || 
-    pathname.startsWith("/api")
-  ) {
-    return NextResponse.next();
-  }
 
   const tokenCookie = req.cookies.get("authToken");
   const token = tokenCookie?.value;
+  const isLoginPage = req.nextUrl.pathname.startsWith("/login");
+  const isSignupPage = req.nextUrl.pathname.startsWith("/signup");
 
-  if (!token) {
+  if (token && (isLoginPage || isSignupPage)) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  if (!token && !(isLoginPage || isSignupPage)) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 
+
+  if (token){
   try {
     verifyToken(token);
-    return NextResponse.redirect(new URL("/", req.url));
+    return NextResponse.next();
+   
   } catch (error) {
     return NextResponse.redirect(new URL("/login", req.url));
   }
 }
+return NextResponse.next();
+}
+
 
 export const config = {
   runtime: "nodejs",
-  matcher: ["/:path*"], 
+  matcher: ["/((?!api|_next|static).*)"],
 };
